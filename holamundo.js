6d126370d6180ee38314e52cb6a53aab3c4cad64
Irving Mtz Btz <L18540374@delicias.tecnm.mx>
2023-04-19 11:52:43 -0600

    holiwis

---
+++ holamundo.js
@@ -0,0 +1,20 @@
+//LLamamos el modulo http
+const http = require('http');
+//Establecemos la url o Ip de nuestro servidor
+const hostname = '192.168.56.1';
+//Establecemos el puerto de escucha
+const port = 3000;
+//Creamos una instancia HTTP con un reques y un response
+const server = http.createServer((req,res)=>{
+    //El servidor respondera un codigo 200
+    res.statusCode = 200;
+    //El servidor respondera con un texto plano
+    res.setHeader('Content-Type','text/plain');
+    //El servidor respondera el mensaje hola mundo
+    res.end('Hola mundo\n');
+
+});
+
+server.listen(port, hostname, () =>{
+    console.log('El servidor se esta ejecutando en http://${hostname}:${port}/');
+});
